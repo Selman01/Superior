e73a5bed4c0cb4eb075c47e0d9f7f72566772c94
Selman01 <memoselo51@gmail.com>
2016-07-28 15:44:53 +0300

    Create model.User.java

---
+++ model.User.java
@@ -0,0 +1,43 @@
+
+package tr.kasim.obs.model;
+
+
+/**
+ *
+ * @author selman
+ */
+public class User {
+
+    String username;
+    String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", password=" + password + '}';
+    }
+
+}
