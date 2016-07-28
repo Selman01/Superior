313f50360a0596a8a79cabeaddb557730296edf2
Selman01 <memoselo51@gmail.com>
2016-07-28 15:10:37 +0300

    Create LoginScreen.java

---
+++ LoginScreen.java
@@ -0,0 +1,38 @@
+
+package tr.kasim.obs.panel;
+
+import tr.kasim.obs.app.Application;
+import tr.kasim.obs.model.User;
+import tr.kasim.obs.util.GeneralUtil;
+
+/**
+ *
+ * @author selman
+ */
+public class LoginScreen extends Screen {
+
+    public LoginScreen(Application application) {
+        super(application);
+    }
+
+    @Override
+    public void showMainScreen() {
+        System.out.println("Welcome to Student Information Screen");
+        System.out.println("User Name : ");
+        String userName = GeneralUtil.readConsole();
+        
+        System.out.println("Password : ");
+        String password = GeneralUtil.readConsole();
+        
+        
+        User user = new User(userName, password);
+        getApplication().login(user);
+
+    }
+
+    @Override
+    public void showFailScreen() {
+        System.out.println("Sorry but username and password doesn't matched.");
+    }
+
+}
